e440c6c8af8b994769c34d5b1cd70084e9602377
natividev <josenatividadcv@gmail.com>
2023-07-17 22:08:56 -0600

    feat: CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import * as morgan from 'morgan';
+import { CORS } from './constants/cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.use(morgan('dev'));
+
+  const configService = app.get(ConfigService);
+
+  app.enableCors(CORS);
+
+  app.setGlobalPrefix('api');
+
+  await app.listen(configService.get('PORT'));
+
+  console.log(`App runnig on ${await app.getUrl()}`);
 }
 bootstrap();
